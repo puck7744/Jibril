19aeb2fc76ae3ac51afc5c90f8ec049065dafb6f
Tristan Shelton <puck7744@gmail.com>
2016-08-08 15:02:17 -0400

    First attempt at permissions

--- Ruby/bot.rb
+++ Ruby/bot.rb
@@ -7,9 +7,13 @@ class Location
   def initialize(server, name, role)
     #Create a custom channel and a role to view it
     @server = server
-    @channel = @server.create_channel(name)
+
     @role = @server.create_role()
     @role.name = role
+
+    @channel = @server.create_channel(name)
+    @channel.define_overwrite(@server.default_role, 0, 9007199254740991) #No permissions
+    @channel.define_overwrite(@role.id, 67177472, 0)#Basic permissions
   end
 
   def finalize()
